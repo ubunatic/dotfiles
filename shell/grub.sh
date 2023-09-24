d4bcb36fa8b672c27936c88bfbd0c76c27f4e797
Uwe <ubunatic@codeberg.org>
2023-09-24 23:46:57 +0200

    add grub func, cleanup tests, passthrough exit code in log funcs

---
+++ shell/grub.sh
@@ -0,0 +1,45 @@
+
+grub_boot_entry() {
+    local grubno
+    grubno="$1"
+    grub_boot_entries | awk -v num="$grubno" '$1 == num { $1=""; print substr($0, 2) }'
+}
+
+grub_boot_entries() {
+    sudo awk -F\' '/menuentry / {count++; print count-1 " " $2}' /boot/grub/grub.cfg
+}
+
+boot_to_grub_entry() {
+    local grubno entry
+
+    grubno="$1"
+
+    if test -z "$grubno"; then
+        grub_boot_entries
+
+        prompt "Chose grub item number to boot"
+        read grubno
+        if test -z "$grubno"
+        then err "grub item number not set"; return 1
+        fi
+    fi
+
+    entry=$(grub_boot_entry "$grubno")
+    if test -z "$entry"
+    then err "grub entry not found for grub item: $grubno"; return 1
+    fi
+
+    if echo "$entry" | grep -q -i windows; then
+        boot_to_windows_warning
+    fi
+
+    ask "boot entry '$entry'?" &&
+    sudo grub-reboot "$grubno" &&
+    sudo reboot
+}
+
+test_grub() {
+    # TODO: find a way to get entries w/o sudo
+    #       then test them here
+    true
+}
---
+++ shell/path.sh
@@ -0,0 +1,5 @@
+
+# add Android SDK platform tools to path
+if test -d "$SYSTEM_APPS/platform-tools"; then
+    PATH="$SYSTEM_APPS/platform-tools:$PATH"
+fi
