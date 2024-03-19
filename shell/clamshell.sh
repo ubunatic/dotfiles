3845f4cf94652a6bcc4c7214985a0462aea736bc
Uwe Jugel <ubunatic@noreply.codeberg.org>
2024-03-19 10:00:37 +0100

    add pmset docs inline, use prefix, add assertions

--- shell/clamshell.sh
+++ shell/clamshell.sh
@@ -44,7 +44,7 @@ Commands:
     sleeping            sln      Check if the system is sleeping (returns 0 if yes)
     awake               aw       Check if the system is awake (returns 0 if yes)
     summary             sm       Display a summary of all checks
-    complete            co       Print the bash/zsh completion function
+    complete            co       Print the zsh completion function
 
 Daemon Commands:
     daemon     dm    Runs the sleep command every second (also see clamshelld)
@@ -58,13 +58,14 @@ Developer Commands:
     selftest   self  Run a selftest to check if all commands work as expected
     log        log   Tail the clamshelld log file
     binary     bin   Compile the clamshell script to a clamshelld binary
+    assertions asn   Show the pmset assertions that prevent sleep
 
 EOF
 }
 
     # Launchd service variables
-    clamshelld_cmd="source $DOTFILES/shell/clamshell.sh && clamshell daemon"
-    clamshelld_bin="$DOTFILES/bin/clamshelld"
+    clamshelld_prefix="$HOME/Library/Clamshell/1.0.0"
+    clamshelld_bin="$clamshelld_prefix/bin/clamshelld"
     clamshelld_service="com.github.ubunatic.clamshell.plist"
     clamshelld_plist="$HOME/Library/LaunchAgents/$clamshelld_service"
     clamshelld_log="$HOME/Library/Logs/clamshell.log"
@@ -73,15 +74,18 @@ EOF
     clamshell-main() {
         # parse flags
         local flag
-        for flag in "$@"; do case "$flag" in
+        for flag in "$@"
+        do case "$flag" in
             -d|--debug)    export CLAMSHELL_DEBUG=1 ;;
             -h|--help|h*)  clamshell-help; return 0 ;;
             -*)            echo "Unknown option: $1"; return 1 ;;
-        esac; done
+        esac
+        done
 
         # run commands
         local cmd
-        for cmd in "$@"; do case "$cmd" in
+        for cmd in "$@"
+        do case "$cmd" in
             -*)            ;;  # ignore flags
             y*|c|ch*)      clamshell-yes ;;
             n|no*)         clamshell-no ;;
@@ -90,20 +94,22 @@ EOF
             dp|de*)        clamshell-proxy-num ;;
             sleepi*|sln)   clamshell-sleeping ;;
             sl*)           clamshell-sleep ;;
-            aw*)           ! clamshell-sleeping ;;
+            aw*)           clamshell-awake ;;
             su*)           clamshell-summary ;;
             da*)           exec -a clamshelld clamshell-daemon | tee -i -a "$clamshelld_log" ;;
+            bin*|compi*)   clamshell-binary ;;
             co*)           clamshell-complete ;;
             in*)           clamshell-install ;;
             un|uni*)       clamshell-uninstall ;;
             st|stat*)      clamshell-status ;;
             log*)          clamshell-log ;;
+            as*)           clamshell-assertions ;;
             ld|lo*|start)  clamshell-ctl load ;;
             ul|unl*|stop)  clamshell-ctl unload ;;
             self*)         clamshell-selftest ;;
-            bin*)          clamshell-binary ;;
             *)             echo "Unknown command: $1"; return 1 ;;
-        esac; done
+        esac
+        done
     }
 
     # clamshell-daemon runs a loop to detect clamshell mode and initiate sleep
@@ -143,8 +149,8 @@ EOF
             if clamshell-sleep; then
                 sleeping_since="$(date +%s)"
                 awake_since=0
-                logger "clamshell sleep initated, waiting 3s to reach sleep state"
-                sleep 3
+                logger "clamshell sleep initated, waiting 5s to reach sleep state"
+                sleep 5
                 continue
             fi
 
@@ -185,8 +191,8 @@ EOF
     }
 
     clamshell-binary() {
-        mkdir -p "$DOTFILES/bin"
-        echo "Compiling clamshell script to clamshelld binary"
+        mkdir -p "$clamshelld_prefix/bin"
+        echo "Compiling clamshell script to clamshelld binary in prefix $clamshelld_prefix"
         (
             echo "#!/usr/bin/env zsh" &&
             which clamshell &&
@@ -196,10 +202,58 @@ EOF
         echo "clamshelld binary created at $clamshelld_bin"
     }
 
+    # man pmset:
+    #      displaysleep             display sleep timer; replaces 'dim' argument in 10.4 (value in minutes, or 0 to disable)
+    #      disksleep                disk spindown timer; replaces 'spindown' argument in 10.4 (value in minutes, or 0 to disable)
+    #      sleep                    system sleep timer (value in minutes, or 0 to disable)
+    #      womp                     wake on ethernet magic packet (value = 0/1). Same as "Wake for network access" in System Settings.
+    #      ring                     wake on modem ring (value = 0/1)
+    #      powernap                 enable/disable Power Nap on supported machines (value = 0/1)
+    #      proximitywake            On supported systems, this option controls system wake from sleep based on proximity of devices using same iCloud id. (value = 0/1)
+    #      autorestart              automatic restart on power loss (value = 0/1)
+    #      lidwake                  wake the machine when the laptop lid (or clamshell) is opened (value = 0/1)
+    #      acwake                   wake the machine when power source (AC/battery) is changed (value = 0/1)
+    #      lessbright               slightly turn down display brightness when switching to this power source (value = 0/1)
+    #      halfdim                  display sleep will use an intermediate half-brightness state between full brightness and fully off  (value = 0/1)
+    #      sms                      use Sudden Motion Sensor to park disk heads on sudden changes in G force (value = 0/1)
+    #      hibernatemode            change hibernation mode. Please use caution. (value = integer)
+    #      hibernatefile            change hibernation image file location. Image may only be located on the root volume. Please use caution. (value = path)
+    #      ttyskeepawake            prevent idle system sleep when any tty (e.g. remote login session) is 'active'. A tty is 'inactive' only when its idle time exceeds the system sleep timer. (value = 0/1)
+    #      networkoversleep         this setting affects how OS X networking presents shared network services during system sleep. This setting is not used by all platforms; changing its value is unsupported.
+    #      destroyfvkeyonstandby    Destroy File Vault Key when going to standby mode. By default File vault keys are retained even when system goes to standby. If the keys are destroyed, user will be prompted to enter the password while coming out of standby mode.(value: 1 - Destroy, 0 - Retain)
+
+    # Testing pmset changes:
+    # sudo pmset -a tcpkeepalive 0   # changes setting, shows a warning
+    # sudo pmset -a ttyskeepawake 0  # changes setting
+    # sudo pmset -a acwake 0         # has no effect
+    # sudo pmset -a sleep 1          # does not change the exclusions for powerd  and bluetoothd
+    # sudo pmset -a ring 0           # has no effect
+
+    # pmset -g before:                                                   # pmset -g after:
+    #  System-wide power settings:                                       #  System-wide power settings:
+    #   SleepDisabled          0                                         #   SleepDisabled          0
+    #   DestroyFVKeyOnStandby          1                                 #   DestroyFVKeyOnStandby          1
+    #  Currently in use:                                                 #  Currently in use:
+    #   standby              1                                           #   standby              1
+    #   Sleep On Power Button 1                                          #   Sleep On Power Button 1
+    #   hibernatefile        /var/vm/sleepimage                          #   hibernatefile        /var/vm/sleepimage
+    #   powernap             1                                           #   powernap             1
+    #   networkoversleep     0                                           #   networkoversleep     0
+    #   disksleep            10                                          #   disksleep            10
+    #   sleep                1 (sleep prevented by powerd, bluetoothd)   #   sleep                1 (sleep prevented by powerd, bluetoothd)
+    #   hibernatemode        3                                           #   hibernatemode        3
+    #   ttyskeepawake        1                                           #   ttyskeepawake        0
+    #   displaysleep         90                                          #   displaysleep         90
+    #   tcpkeepalive         1                                           #   tcpkeepalive         0
+    #   lowpowermode         0                                           #   lowpowermode         0
+    #   womp                 0                                           #   womp                 0
+
     clamshell-yes()         { ioreg -r -k AppleClamshellState | grep AppleClamshellState | grep -q "Yes"; }
     clamshell-no()          { ! clamshell-yes; }
     clamshell-log()         { tail -F "$clamshelld_log"; }
     clamshell-sleeping()    { pmset -g assertions | grep -qE '^\s*PreventUserIdleSystemSleep\s*0'; }
+    clamshell-awake()       { pmset -g assertions | grep -qE '^\s*PreventUserIdleSystemSleep\s*1'; }
+    clamshell-assertions()  { pmset -g assertions | grep -E  'PreventUserIdleSystemSleep'; }
     clamshell-proxy-num()   { pmset -g powerstate | grep -c DCPDPDeviceProxy; }
     clamshell-has-display() { test "$(clamshell-proxy-num)" -lt 4; }
     clamshell-has-legacy() {
@@ -218,8 +272,8 @@ EOF
         echo "clamshell-sleep:       $(CLAMSHELL_DEBUG=1 clamshell-sleep)"
     }
 
-    logger-n() { echo -n -e "\r$(date '+%Y-%m-%d %H:%M:%S'): $*, output="; }  # log without newline
-    logger()   { echo    -e "\r$(date '+%Y-%m-%d %H:%M:%S'): $*"; }           # log with newline
+    logger-n()     { echo -n -e "\r$(date '+%Y-%m-%d %H:%M:%S'): $*, output="; }  # log without newline
+    logger()       { echo    -e "\r$(date '+%Y-%m-%d %H:%M:%S'): $*"; }           # log with newline
 
     # shellcheck disable=SC2317
     echo-pmset() { echo "/usr/bin/pmset $*"; }
@@ -236,14 +290,15 @@ EOF
         if clamshell-yes; then
             code=0
             if clamshell-has-display; then
-                logger-n "clamshell detected, display found"
+                logger-n "clamshell detected, display found, initating sleep cmd=$pmset arg=sleepnow"
                 $pmset sleepnow
                 code=$?
             elif clamshell-has-legacy; then
-                logger-n "clamshell detected, legacy display found"
+                logger-n "clamshell detected, legacy display found, initating sleep cmd=$pmset arg=sleepnow"
                 $pmset sleepnow
                 code=$?
             elif test -n "$CLAMSHELL_DEBUG"; then
+                # echo a noop command to stdout as sleep command output for testing
                 echo noop "(lid open or display asleep)"
             fi
 
@@ -260,7 +315,7 @@ EOF
 
     # clamshell-install installs a Launchd service to run clamshelld in the background.
     clamshell-install() {
-        local svc="$clamshelld_service" dst="$clamshelld_plist" cmd="$clamshelld_cmd"
+        local svc="$clamshelld_service" dst="$clamshelld_plist"
 
         if ! clamshell-binary
         then echo "Failed to create clamshelld binary"; return 1
@@ -302,7 +357,7 @@ EOF
             return 1
         fi
 
-        echo "Launchd service $svc installed at $dst using cmd='$cmd'"
+        echo "Launchd service $svc installed at $dst using binary '$clamshelld_bin'"
         echo "Stopping clamshelld instances and reloading clamshelld service"
         if pkill clamshelld 2> /dev/null
         then echo "clamshelld process stopped"
@@ -357,18 +412,20 @@ EOF
 
         local err=""
         err()       { err="$err\n$*"; }
-        noerr()     { wc -l | grep -q -E '^ *0' || err "$@"; }
+        noerr()     { wc -l | grep -qE '^\s*0' || err "$@"; }
         nobasherr() { grep -E '^(bash:|clamshell*:)' && err "$@"; }
 
         clamshell-yes || clamshell-no  || err "clamshell yes/no failed"
         clamshell-complete  >/dev/null || err "clamshell complete failed"
         clamshell-proxy-num >/dev/null || err "clamshell proxy-num failed"
-        # commands without output should show no errors
+        clamshell-awake                || err "clamshell awake failed"
+        ! clamshell-sleeping           || err "clamshell sleeping failed"
+        # commands without output should not show any errors
         # do not run these tests in clamshell mode
         clamshell-has-display | noerr "clamshell has-display failed"
         clamshell-has-legacy  | noerr "clamshell has-legacy failed"
         clamshell-sleep       | noerr "clamshell sleep failed"
-        # commands with output should show no bash errors
+        # commands with output should not show any bash errors
         clamshell-summary          | nobasherr "clamshell summary failed"
         clamshell-help             | nobasherr "clamshell help failed"
         clamshell-status           | nobasherr "clamshell status failed"
